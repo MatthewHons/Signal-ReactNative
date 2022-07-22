6962c78ce230fe2abb0ba7ebfc4dc1d9d694813a
Matthew Hons <devwithmatt@gmail.com>
2022-07-22 07:57:42 +0200

    Adding the react-native-navigation dependencies and firebase credentials

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCB1MFsCyL90xRBbJNzhr9kSuBTNK4Xd_4",
+  authDomain: "signal-42c67.firebaseapp.com",
+  projectId: "signal-42c67",
+  storageBucket: "signal-42c67.appspot.com",
+  messagingSenderId: "682780245650",
+  appId: "1:682780245650:web:a1312f56030076dd85b04b",
+};
